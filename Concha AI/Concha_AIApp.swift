ac22d7980de5862272464d89262d77f468c097af
GianGuaz256 <guazzogianmarco@gmail.com>
2025-12-02 15:03:31 +0100

    Initial Commit

---
+++ Concha AI/Concha_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concha_AIApp.swift
+//  Concha AI
+//
+//  Created by Gianmarco Guazzo on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Concha_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
